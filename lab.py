042bd1673873fb7de564f8b91e9ada16b037dd07
taras-ua <tarasrogov@gmail.com>
2014-04-16 17:43:12 +0300

    hw done

--- lab.py
+++ lab.py
@@ -19,7 +19,7 @@ def area_size():
 
 # Is point in area
 def is_in_area(point):
-    return 4.0 * point[0] + point[1] + point[2]
+    return scalar(point, area())
 
 
 # Derivative by parameter (0 for x, 1 for y, 2 for z)
@@ -36,31 +36,6 @@ def derivative_by(param, var, eps=0.001):
 # Gradient of target function
 def grad(var, eps=0.001):
     return np.array([derivative_by(0, var, eps), derivative_by(1, var, eps), derivative_by(2, var, eps)])
-
-
-# Second derivative by same parameter (ex. d^2f/dx^2)
-def sec_derivative_by_param(param, var, eps=0.001):
-    var_plus_eps = np.array(var)
-    var_plus_eps[param] = var[param] + eps
-    var_minus_eps = np.array(var)
-    var_minus_eps[param] = var[param] - eps
-    result = target_func(var_plus_eps) - 2 * target_func(var) + target_func(var_minus_eps)
-    result /= eps ** 2
-    return result
-
-
-# Second derivative by different parameters (ex. d^2f/dx*dz)
-def sec_derivative_by_two_params(param1, param2, var, eps=0.001):
-    var1_plus_eps = np.array(var)
-    var1_plus_eps[param1] = var[param1] + eps
-    var2_plus_eps = np.array(var)
-    var2_plus_eps[param2] = var[param2] + eps
-    var_both_plus_eps = np.array(var)
-    var_both_plus_eps[param1] = var1_plus_eps[param1]
-    var_both_plus_eps[param2] = var2_plus_eps[param2]
-    result = target_func(var_both_plus_eps) - target_func(var1_plus_eps) - target_func(var2_plus_eps) + target_func(var)
-    result /= eps ** 2
-    return result
 
 
 # Vector norm
@@ -83,7 +58,7 @@ def step_grind(var, grad_var):
 
 # Projection of point
 def projection(var):
-    return var + (area_size() - scalar(area(), var)) * area() / vector_norm(area())**2
+    return var + (area_size() - is_in_area(var)) * area() / vector_norm(area())**2
 
 
 # Print iteration
@@ -119,8 +94,9 @@ def main():
     y0 = input("y0 = ")
     z0 = input("z0 = ")
     var0 = np.array([x0, y0, z0])
-    res = gradient_method_with_projection(var0, 0.00001)
+    res = gradient_method_with_projection(var0, 0.00001, True)
     print ""
+    print "======= RESULT ======="
     print "MIN =", res
     print "F min =", target_func(res)
 
